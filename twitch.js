cd8c1c30c060b24a8f218c6d9307d7635d4226cd
Spiffyk <oto.stava@gmail.com>
2016-03-20 11:10:34 +0100

    Added brief YUIDoc documentation

--- twitch.js
+++ twitch.js
@@ -1,3 +1,14 @@
+/**
+ * This is the module to be used in Node.js, NW.js or Electron
+ * @module twitch-sdk
+ */
+
+/**
+ * This is the main class of the SDK. Methods marked as `public` are exposed
+ * to your app.
+ * @class Twitch
+ */
+
 const util = require('./lib/twitch.util');
 const core = require('./lib/twitch.core');
 const init = require('./lib/twitch.init');
@@ -18,8 +29,8 @@ exports.events = {
   getMaxListeners: core.events.getMaxListeners,
   listenerCount: core.events.listenerCount
 };
-exports.addListener = core.events.on;
 exports.on = core.events.on;
+exports.addListener = core.events.on;
 exports.once = core.events.once;
 exports.removeListener = core.events.removeListener;
 exports.getMaxListeners = core.events.getMaxListeners;
